a1f55faf83548afaa56eb3059332235d5fca8641
yangyueren <yangyueren@outlook.com>
2021-09-19 14:06:46 +0000

    debug bcache

--- kernel/bio.c
+++ kernel/bio.c
@@ -42,24 +42,25 @@ binit(void)
 
   initlock(&bcache.lock, "bcache");
   for (int i = 0; i < NUM_BUCKET; i++){
-    initlock(&(bcache.bucket_locks[i]), "bcache");
+    initlock(&(bcache.bucket_locks[i]), "bcache.bucket");
   }
 
   // Create linked list of buffers
   for (int i = 0; i < NUM_BUCKET; i++){
-    bcache.bucket_heads[i].prev = &bcache.bucket_heads[i];
-    bcache.bucket_heads[i].next = &bcache.bucket_heads[i];
+    bcache.bucket_heads[i].prev = &(bcache.bucket_heads[i]);
+    bcache.bucket_heads[i].next = &(bcache.bucket_heads[i]);
   }
 
-  if(NUM_BUCKET < NBUF){
+  if(NUM_BUCKET > NBUF){
     panic("binit");
   }
 
   int j = 0;
   for (b = bcache.buf; b < bcache.buf + NBUF; b++) {
     b->ticks = ticks;
+    b->refcnt = 0;
     b->next = bcache.bucket_heads[j].next;
-    b->prev = &bcache.bucket_heads[j];
+    b->prev = &(bcache.bucket_heads[j]);
     initsleeplock(&b->lock, "buffer"); //every buffer has a sleeplock
     bcache.bucket_heads[j].next->prev = b;
     bcache.bucket_heads[j].next = b;
@@ -90,14 +91,17 @@ bget(uint dev, uint blockno)
       return b;
     }
   }
+  release(&bcache.bucket_locks[bucket_id]);
 
 
   struct buf * lru = (struct buf *)(0);
   int lru_bucket = -1;
 
+  
   //borrow one lru unused buffer
+find_LRU:
   for (int i = 0; i < NUM_BUCKET; i++){
-    for (b = bcache.bucket_heads[i].prev; b != &bcache.bucket_heads[i]; b = b->prev){
+    for (b = bcache.bucket_heads[i].prev; b != &(bcache.bucket_heads[i]); b = b->prev){
       if(b->refcnt == 0){
           if(!lru){
             lru = b;
@@ -111,10 +115,25 @@ bget(uint dev, uint blockno)
       }
     }
   }
-
   if(lru_bucket == -1){
     panic("bget: no buffers");
   }
+  if(lru_bucket == bucket_id){
+    acquire(&bcache.bucket_locks[lru_bucket]);
+    if(lru->refcnt != 0){
+      release(&bcache.bucket_locks[lru_bucket]);
+      goto find_LRU;
+    }
+  }else{
+    acquire(&bcache.bucket_locks[bucket_id]);
+    acquire(&bcache.bucket_locks[lru_bucket]);
+    if(lru->refcnt != 0){
+      release(&bcache.bucket_locks[bucket_id]);
+      release(&bcache.bucket_locks[lru_bucket]);
+      goto find_LRU;
+    }
+  }
+
   //delete lru from original bucket
   if(lru_bucket == bucket_id){
       lru->dev = dev;
@@ -122,28 +141,30 @@ bget(uint dev, uint blockno)
       lru->valid = 0;
       lru->refcnt = 1;
       lru->ticks = ticks;
+      release(&bcache.bucket_locks[lru_bucket]);
       release(&bcache.lock);
       acquiresleep(&lru->lock);
       return lru;
   }else{
-    acquire(&bcache.bucket_locks[lru_bucket]);
+    
     lru->next->prev = lru->prev;
     lru->prev->next = lru->next;
-    release(&bcache.bucket_locks[lru_bucket]);
-
+    
     lru->next = bcache.bucket_heads[bucket_id].next;
     lru->prev = &bcache.bucket_heads[bucket_id];
     bcache.bucket_heads[bucket_id].next->prev = lru;
     bcache.bucket_heads[bucket_id].next = lru;
 
     lru->dev = dev;
-      lru->blockno = blockno;
-      lru->valid = 0;
-      lru->refcnt = 1;
-      lru->ticks = ticks;
-      release(&bcache.lock);
-      acquiresleep(&lru->lock);
-      return lru;
+    lru->blockno = blockno;
+    lru->valid = 0;
+    lru->refcnt = 1;
+    lru->ticks = ticks;
+    release(&bcache.bucket_locks[lru_bucket]);
+    release(&bcache.bucket_locks[bucket_id]);
+    release(&bcache.lock);
+    acquiresleep(&lru->lock);
+    return lru;
 
   }
 
@@ -194,14 +215,22 @@ brelse(struct buf *b)
 void
 bpin(struct buf *b) {
   acquire(&bcache.lock);
+  int bucket_id = b->blockno % NUM_BUCKET;
+  acquire(&bcache.bucket_locks[bucket_id]);
+
   b->refcnt++;
+  release(&bcache.bucket_locks[bucket_id]);
   release(&bcache.lock);
 }
 
 void
 bunpin(struct buf *b) {
   acquire(&bcache.lock);
+  int bucket_id = b->blockno % NUM_BUCKET;
+  acquire(&bcache.bucket_locks[bucket_id]);
+
   b->refcnt--;
+  release(&bcache.bucket_locks[bucket_id]);
   release(&bcache.lock);
 }
 
